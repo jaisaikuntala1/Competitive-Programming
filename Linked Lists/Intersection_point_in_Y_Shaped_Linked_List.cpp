2b04c80a32ecfca20eec9f33212c066976323df0
K JAISAI <jaisaikuntala1@gmail.com>
2021-01-03 08:32:06 +0530

    Intersection point in a Y shaped linked list solution added

---
+++ Linked Lists/Intersection_point_in_Y_Shaped_Linked_List.cpp
@@ -0,0 +1,130 @@
+/*
+
+Intersection Point in Y Shapped Linked Lists 
+
+Given two singly linked lists of size N and M, write a program to get the point where two linked lists intersect each other.
+
+ 
+
+Example 1:
+
+Input:
+LinkList1 = 3->6->9->common
+LinkList2 = 10->common
+common = 15->30->NULL
+Output: 15
+Explanation:
+Y ShapedLinked List
+ 
+
+Your Task:
+You don't need to read input or print anything. The task is to complete the function intersetPoint() which takes the pointer to the head of linklist1(head1) and linklist2(head2) as input parameters and returns data value of a node where two linked lists intersect. If linked list do not merge at any point, then it should return -1.
+Challenge : Try to solve the problem without using any extra space.
+
+ 
+
+Expected Time Complexity: O(N+M)
+Expected Auxiliary Space: O(1)
+
+ 
+
+Constraints:
+1 <= N <= 100
+1 <= value <= 1000
+
+*/
+
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+int intersectPoint(struct Node* head1, struct Node* head2);
+
+Node* inputList(int size)
+{
+    if(size==0) return NULL;
+    
+    int val;
+    cin>> val;
+    
+    Node *head = new Node(val);
+    Node *tail = head;
+    
+    for(int i=0; i<size-1; i++)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+
+
+/* Linked List Node
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+/* Should return data of intersection point of two linked
+   lists head1 and head2.
+   If there is no intersecting point, then return -1. */
+int intersectPoint(Node* head1, Node* head2)
+{
+    int s1 = 0;
+    int s2 = 0;
+    struct Node* temp1 = head1;
+    struct Node* temp2 = head2;
+    while(temp1!=NULL){
+        s1++;
+        temp1=temp1->next;
+    }
+        while(temp2!=NULL){
+        s2++;
+        temp2=temp2->next;
+    }
+    
+    int diff = abs(s1-s2);
+    if(s1>s2){
+        
+        while(diff--){
+            head1 = head1->next;
+        }
+    }
+    else if(s2>s1){
+    
+        while(diff--){
+            head2 = head2->next;
+        }
+    }
+    else{
+        
+    }
+    while(head1!=NULL && head2!=NULL){
+        if(head1 == head2){
+            return head1->data;
+        }
+        head1 = head1->next;
+        head2 = head2->next;
+    }
+    
+}
+
