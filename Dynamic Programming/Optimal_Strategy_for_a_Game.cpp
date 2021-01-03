e9a4cd88563f1f52a44f382757683803127991b7
K JAISAI <jaisaikuntala1@gmail.com>
2021-01-03 08:24:36 +0530

    Solution added for the Problem Optimal Strategy for a Game

--- Dynamic Programming/Optimal_Strategy_for_a_Game.cpp
+++ Dynamic Programming/Optimal_Strategy_for_a_Game.cpp
@@ -33,3 +33,60 @@ Constraints:
 2 <= N <= 100
 1 <= Ai <= 106
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+long long dp[102][102];
+
+long long maxscore(int arr[],int i,int j){
+    if(dp[i][j]!=-1){
+        return dp[i][j];
+    }
+    if(j-i==0){
+        return dp[i][j] = arr[i];
+    }
+    if(j-i == 1){
+        return dp[i][j] = max(arr[i],arr[j]);
+    }
+    int op1 = arr[i] + min(maxscore(arr,i+2,j),maxscore(arr,i+1,j-1));
+    int op2 = arr[j] + min(maxscore(arr,i+1,j-1),maxscore(arr,i,j-2));
+    
+    return dp[i][j] = max(op1,op2);
+    
+}
+long long maximumAmount(int arr[], int n) 
+{
+    // Your code here
+    memset(dp,-1,sizeof dp);
+    return maxscore(arr,0,n-1);
+}
+
+// { Driver Code Starts.
+
+int main() {
+    
+    //taking total testcases
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    //taking number of elements
+	    int N;
+	    cin>>N;
+	    
+	    int A[N];
+	    
+	    //inserting the elements
+	    for(int i=0;i<N;i++)
+	    cin>>A[i];
+	    
+	    //calling function maximumAmount()
+	    cout<<maximumAmount(A,N)<<endl;
+	    
+	}
+	return 0;
+}  // } Driver Code Ends
