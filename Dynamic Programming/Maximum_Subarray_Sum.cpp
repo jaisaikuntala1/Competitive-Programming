704a4edd480d929af1461d52966974edc3ad83b2
K JAISAI <jaisaikuntala1@gmail.com>
2021-01-04 05:04:30 +0530

    Maximum Subarray Sum Solution Added

--- Dynamic Programming/Maximum_Subarray_Sum.cpp
+++ Dynamic Programming/Maximum_Subarray_Sum.cpp
@@ -43,3 +43,49 @@ Constraints:
 
 */
 
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+// Function to find subarray with maximum sum
+// arr: input array
+// n: size of array
+int maxSubarraySum(int arr[], int n){
+    
+    // Your code here
+    int ans = 0;
+    int dp[n+1];
+    dp[0] = max(0,arr[0]);
+    ans = dp[0];
+    for(int i=1;i<n;i++){
+        dp[i] = arr[i]+max(0,dp[i-1]);
+        ans = max(ans,dp[i]);
+    }
+    return ans;
+    
+}
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+        
+        cout << maxSubarraySum(a, n) << endl;
+    }
+}
+  // } Driver Code Ends
