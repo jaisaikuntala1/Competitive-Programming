acb9d5aab55c7b08722df08a8c3aeb15b4b2a3b3
K JAISAI <jaisaikuntala1@gmail.com>
2021-01-04 05:07:48 +0530

    Count number of hops problem added

--- Dynamic Programming/Count_number_of_hops.cpp
+++ Dynamic Programming/Count_number_of_hops.cpp
@@ -33,3 +33,46 @@ Constraints:
 1 ≤ N ≤ 105
 
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+long long countWays(int);
+ 
+int main()
+{
+    //taking testcases
+	int t;
+	cin >> t;
+	
+	while(t--)
+	{
+	    //taking number of steps in stair
+		int n;
+		cin>>n;
+		
+		//calling function countWays()
+		cout << countWays(n) << endl;
+	}
+    
+    return 0;
+    
+}
+// } Driver Code Ends
+
+
+// function to count number of ways to reach top of the stair
+long long countWays(int n){
+    long long dp[n+1];
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+    for(long long i=4;i<=n;i++){
+        dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+        dp[i] %= 1000000007;
+        
+    }
+    return dp[n];
+    
+    
+}
