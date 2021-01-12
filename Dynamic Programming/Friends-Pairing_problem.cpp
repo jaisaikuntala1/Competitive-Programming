5f2f6c7f3f1be22506dd5cb75244d570a202c3be
K JAISAI <jaisaikuntala1@gmail.com>
2021-01-12 14:53:49 +0530

    Friends Pairing Problem

--- Dynamic Programming/Friends-Pairing_problem.cpp
+++ Dynamic Programming/Friends-Pairing_problem.cpp
@@ -1,5 +1,7 @@
 /*
 
+FRIENDS PAIRING PROBLEM :
+
 
 Given n friends, each one can remain single or can be paired up with some other friend. Each friend can be paired only once. Find out the total number of ways in which friends can remain single or can be paired up.
 
