263f01af98584a6976b790fbb3450058893f5f35
K JAISAI <jaisaikuntala1@gmail.com>
2021-01-12 14:54:35 +0530

    Friends Pairing Problem Solution

--- Dynamic Programming/Friends-Pairing_problem.cpp
+++ Dynamic Programming/Friends-Pairing_problem.cpp
@@ -26,3 +26,27 @@ Explanation
 
 
 */
+
+//CODE :
+
+#include<iostream>
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+	int t;cin>>t;
+	while(t--){
+		int n;cin>>n;
+		int dp[n+1];
+		dp[0] = 1;
+		dp[1] = 1;
+		for(int i=2;i<=n;i++){
+			dp[i] = dp[i-1]+(i-1)*dp[i-2];
+		}
+		cout<<dp[n]<<endl;
+	}
+
+
+	return 0;
+}
