205e18a92c347548da033cc96a4e0f94409a6c46
K JAISAI <jaisaikuntala1@gmail.com>
2021-01-13 13:44:20 +0530

    Inversion Count

--- Divide and Conquer/Inversion_Count.cpp
+++ Divide and Conquer/Inversion_Count.cpp
@@ -28,14 +28,9 @@ Sample Output
 
 
 
-#include <iostream>
 #include <bits/stdc++.h>
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
 using namespace std;
-using namespace __gnu_pbds;
 #define int long long
-typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> PBDS;
 
 int inv_cnt;
 
