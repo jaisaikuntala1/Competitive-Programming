886216a9938092c811ea35be368b0f4ba622e1ad
K JAISAI <jaisaikuntala1@gmail.com>
2021-01-13 13:42:04 +0530

    Inversion Count

---
+++ Divide and Conquer/Inversion_Count.cpp
@@ -0,0 +1,24 @@
+/*
+
+You are given an array A find out number of pairs (i,j) in array A such that A[i] > A[j] and i < j.
+
+Input Format
+The first line consists of number of test cases t. Each test case consists of integer N followed by N space separated integers.
+
+Constraints
+1 <= N <= 100000 1 <= t <= 20 0 <= A[i] <= 100000000
+
+Output Format
+The number of inversions in array
+
+Sample Input
+2
+4
+1 2 4 3
+3
+3 2 1
+Sample Output
+1
+3
+
+*/
