82b1fcf3654ead1f0094883a6b2bb402394b28e7
K JAISAI <jaisaikuntala1@gmail.com>
2021-01-17 04:57:27 +0530

    Minimum cost of ropes solution

--- Greedy/Minimum Cost of ropes.cpp
+++ Greedy/Minimum Cost of ropes.cpp
@@ -49,3 +49,50 @@ Constraints:
 1 ≤ arr[i] ≤ 106
 
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Position this line where user code will be pasted.
+long long minCost(long long arr[], long long n);
+int main() {
+    long long t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        cin >> n;
+        long long i, a[n];
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        cout << minCost(a, n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+long long minCost(long long arr[], long long n) {
+    // Your code here
+    priority_queue<long long, vector<long long>, greater<long long>> pq;
+    for(long long i=0;i<n;i++){
+        pq.push(arr[i]);
+    }
+    long long ans = 0;
+    while(pq.size()>1){
+        long long x = pq.top();
+        ans += pq.top();
+        pq.pop();
+        long long y = pq.top();
+        ans += pq.top();
+        pq.pop();
+        pq.push(x+y);
+        
+        
+    }
+    return ans;
+    
+    
+    
+    
+}
