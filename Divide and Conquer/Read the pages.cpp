b7936960325124ef6cac49f59a8653f67717da1f
K JAISAI <jaisaikuntala1@gmail.com>
2021-01-19 18:45:46 +0530

    Read the pages

---
+++ Divide and Conquer/Read the pages.cpp
@@ -0,0 +1,21 @@
+/*
+
+You are given N books, ith of which has Pi pages (Pi <= Pj, if i < j). You have to assign these N books to M students, such that each student has subsegment of books and the maximum number of pages assigned to a student is minimized.
+You have to find the maximum number of pages, a student can have in this assignment of books.
+
+Input Format
+First line contains integers, N and M, denoting the number of books and number of students respectively. Next line contains N integers, denoting the number of pages in N books.
+
+Constraints
+1<=N<=10^5 1<=M<=N 1<=Pi<=10^9
+
+Output Format
+Print a single integer, denoting the answer.
+
+Sample Input
+4 2
+10 20 30 40
+Sample Output
+60
+
+*/
