6ca3cc71ff0d3dd69d16a2478766e251888e4abc
K JAISAI <jaisaikuntala1@gmail.com>
2021-01-26 07:02:29 +0530

    unit area of largest region of 1's solution

--- Graphs/Unit Area of largest region of 1's.cpp
+++ Graphs/Unit Area of largest region of 1's.cpp
@@ -36,3 +36,93 @@ Constraints:
 1 ≤ n, m ≤ 500
 
 */
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+int maxi;
+int n,m;
+int a[501][501];
+int visited[501][501];
+
+void dfs(int x,int y){
+    for(int i=x-1;i<=x+1;i++){
+        for(int j=y-1;j<=y+1;j++){
+            if(i<0 || i>n-1 || j<0 || j>m-1){
+                continue;
+            }
+            if(a[i][j] == 0){
+                visited[i][j] = 1;
+                continue;
+            }
+            if(visited[i][j] == 1){
+                continue;
+            }
+            visited[i][j] = 1;
+            maxi++;
+            if(i!=x || j!=y){
+                dfs(i,j);
+            }
+        }
+    }
+}
+
+class Solution {
+public:
+    int findMaxArea(vector<vector<int>>& grid) {
+        // Code here
+        int ans = INT_MIN;
+        int N = grid.size();
+        int M = grid[0].size();
+        
+    n = N;
+    m = M;
+    memset(a,0,sizeof a);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            a[i][j] = grid[i][j];
+        }
+    }
+   memset(visited,0,sizeof visited);
+   for(int i=0;i<n;i++){
+       for(int j=0;j<m;j++){
+           if(a[i][j] == 0){
+               visited[i][j] = 1;
+               continue;
+           }
+           if(visited[i][j]==1){
+               continue;
+           }
+           maxi = 0;
+           dfs(i,j);
+           ans = max(ans,maxi);
+           
+       }
+   }
+return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>>grid(n, vector<int>(m, -1));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				cin >> grid[i][j];
+			}
+		}
+		Solution obj;
+		int ans = obj.findMaxArea(grid);
+		cout << ans << "\n";
+	}
+	return 0;
+}  // } Driver Code Ends
