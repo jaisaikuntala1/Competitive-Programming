6965dd4c8cabb7105cc3983fd3299c81514d1f32
K JAISAI <jaisaikuntala1@gmail.com>
2021-02-03 12:34:49 +0530

    Knapsack 2 Solution

--- Dynamic Programming/Knapsack 2.cpp
+++ Dynamic Programming/Knapsack 2.cpp
@@ -172,3 +172,56 @@ Items
 .
 
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int32_t main() {
+    int n,W;
+    cin>>n>>W;
+    int wt[n];
+    int val[n];
+    for(int i=0;i<n;i++){
+        cin>>wt[i]>>val[i];
+    }
+    int dp[n+1][100002];
+    memset(dp,-1,sizeof dp);
+    for(int i=0;i<=n;i++){
+        dp[i][0] = 0;
+
+    }
+    for(int i=1;i<=val[0];i++){
+        dp[1][i] = wt[0];
+    }
+    for(int i=val[0]+1;i<=100000;i++){
+        dp[1][i] = INT_MAX;
+    }
+    int summ = val[0];
+    for(int i=2;i<=n;i++){
+        summ += val[i-1];
+        for(int j=1;j<=summ;j++){
+            dp[i][j] = dp[i-1][j];
+            if(j>=val[i-1]){
+                dp[i][j] = min(dp[i][j],wt[i-1]+dp[i-1][j-val[i-1]]);
+            }
+            else{
+                dp[i][j] = min(dp[i][j],wt[i-1]);
+            }
+        }
+        for(int j=summ+1;j<=100000;j++){
+            dp[i][j] = INT_MAX;
+        }
+    }
+    int ans = INT_MIN;
+
+    for(int i = summ;i>=0;i--){
+        if(dp[n][i]<=W){
+            ans = max(ans,i);
+
+        }
+
+    }
+    cout<<ans;
+ 
+  
+}
