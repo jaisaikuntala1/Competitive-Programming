382f38cae7625654528f9880424f14b081661dca
K JAISAI <jaisaikuntala1@gmail.com>
2021-02-03 06:43:14 +0530

    Grid 1 Solution

--- Dynamic Programming/Grid 1 .cpp
+++ Dynamic Programming/Grid 1 .cpp
@@ -270,3 +270,50 @@ Be sure to print the count modulo
 
 
 */
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define int long long
+int32_t main() {
+    int m,n;
+    cin>>m>>n;
+    char a[m][n];
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cin>>a[i][j];
+        }
+    }
+    int dp[m][n];
+    memset(dp,0,sizeof dp);
+    int i = 0;
+    for(i = 0;i<m;i++){
+        if(a[i][0]=='#'){
+            break;
+        }
+        dp[i][0] = 1;
+    }
+    for(int j = i;j<m;j++){
+        dp[j][0] = 0;
+    }
+    i=0;
+    for(i=0;i<n;i++){
+        if(a[0][i]=='#'){
+            break;
+        }
+        dp[0][i] = 1;
+    }
+    for(int j=i;j<n;j++){
+        dp[0][j] = 0;
+    }
+    for(int i=1;i<m;i++){
+        for(int j=1;j<n;j++){
+            if(a[i][j]=='#') {continue;}
+            else{
+                dp[i][j] = (dp[i-1][j]+dp[i][j-1])%mod;
+            }
+        }
+    }
+    cout<<dp[m-1][n-1];
+}
