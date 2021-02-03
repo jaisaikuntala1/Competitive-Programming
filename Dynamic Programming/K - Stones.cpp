b38925e8871c53d1bc598b218a922c4ea142f1d0
K JAISAI <jaisaikuntala1@gmail.com>
2021-02-03 06:02:22 +0530

    K - Stones Solution

--- Dynamic Programming/K - Stones.cpp
+++ Dynamic Programming/K - Stones.cpp
@@ -140,3 +140,29 @@ Sample Output 6
 Second
 
 */
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n,k;
+    cin>>n>>k;
+    int a[n];
+    for(int i=0;i<n;i++) {cin>>a[i];}
+    int dp[k+1]={0};
+    for(int i=1;i<=k;i++){
+        for(int j=1;j<=n;j++){
+            if(a[j-1]<=i){
+                if(dp[i-a[j-1]] == 0){
+                    dp[i] = 1;
+                }
+            }
+        }
+    }
+    if(dp[k]==1){
+        cout<<"First"<<endl;
+    }
+    else{
+        cout<<"Second"<<endl;
+    }
+}
