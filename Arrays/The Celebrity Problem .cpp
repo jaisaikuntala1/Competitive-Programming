b21641486f0e2897d729301122313a41236c0c26
K JAISAI <jaisaikuntala1@gmail.com>
2021-02-06 12:24:02 +0530

    The Celebrity Problem

--- Arrays/The Celebrity Problem .cpp
+++ Arrays/The Celebrity Problem .cpp
@@ -43,3 +43,80 @@ Constraints:
 
 */
 
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+//User function template for C++
+
+class Solution {
+    public:
+    int celebrity(vector<vector<int> >& M, int n) {
+        // code here 
+        
+        stack<int> st;
+        for(int i=0;i<n;i++){
+            st.push(i);
+        }
+        while(st.size()>1){
+            int a = st.top();
+            st.pop();
+            int b = st.top();
+            st.pop();
+            if(M[a][b] && !M[b][a]){
+                st.push(b);
+                continue;
+            }
+            else if(M[b][a] && !M[a][b]){
+                st.push(a);
+                continue;
+            }
+            else{
+                
+            }
+        }
+        if(st.size()==0){
+            return -1;
+        }
+        int celeb = st.top();
+        for(int i=0;i<n;i++){
+            if(i==celeb){
+                continue;
+            }
+            if(!M[i][celeb] || M[celeb][i]){
+                return -1;
+            }
+        }
+        return celeb;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<vector<int> > M( n , vector<int> (n, 0));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                cin>>M[i][j];
+            }
+        }
+        Solution ob;
+        cout<<ob.celebrity(M,n)<<endl;
+
+    }
+}
+  // } Driver Code Ends
+
