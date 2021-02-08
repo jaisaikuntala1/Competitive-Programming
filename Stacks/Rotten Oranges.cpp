fa9cdbce457ca01fa63de482d8be03d3abcf7a10
K JAISAI <jaisaikuntala1@gmail.com>
2021-02-08 21:20:10 +0530

    Rotten Oranges

--- Stacks/Rotten Oranges.cpp
+++ Stacks/Rotten Oranges.cpp
@@ -44,3 +44,126 @@ Constraints:
 1 ≤ n, m ≤ 500
 
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        // Code here
+        int num_1 = 0;
+        int num_2 = 0;
+        int num_0 = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        queue<pair<int,int>> q;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==0){
+                    num_0++;
+                }
+                else if(grid[i][j]==1){
+                    num_1++;
+                }
+                else{
+                    num_2++;
+                    q.push({i,j});
+                }
+            }
+        }
+        if(num_1==0){
+            return 0;
+        }
+        
+        int min_time = 0;
+        int num_rotten = 0;
+        while(1){
+            bool atleast_one_changed = false;
+            min_time++;
+            int s = q.size();
+            while(s--){
+                pair<int,int> p = q.front();
+                int i = p.first;
+                int j = p.second;
+                if(i-1>=0){
+                    if(grid[i-1][j]==1){
+                        grid[i-1][j] = 2;
+                        q.push({i-1,j});
+                        num_rotten++;
+                        atleast_one_changed = true;
+                        if(num_rotten==num_1){
+                            
+                            return min_time;
+                        }
+                    }
+                }
+                if(j+1<n){
+                    if(grid[i][j+1]==1){
+                        grid[i][j+1] = 2;
+                        q.push({i,j+1});
+                        num_rotten++;
+                        atleast_one_changed = true;
+                        if(num_rotten==num_1){
+                            return min_time;
+                        }
+                    }
+                    
+                }
+                if(i+1<m){
+                     if(grid[i+1][j]==1){
+                        grid[i+1][j] = 2;
+                        q.push({i+1,j});
+                        num_rotten++;
+                        atleast_one_changed = true;
+                        if(num_rotten==num_1){
+                            return min_time;
+                        }
+                    }
+                    
+                }
+                if(j-1>=0){
+                    if(grid[i][j-1]==1){
+                        grid[i][j-1] = 2;
+                        q.push({i,j-1});
+                        num_rotten++;
+                        atleast_one_changed = true;
+                        if(num_rotten==num_1){
+                            return min_time;
+                        }
+                    }
+                    
+                }
+                q.pop();
+                
+            }
+            if(atleast_one_changed==false){
+                return -1;
+            }
+        }
+        
+    }
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>>grid(n, vector<int>(m, -1));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				cin >> grid[i][j];
+			}
+		}
+		Solution obj;
+		int ans = obj.orangesRotting(grid);
+		cout << ans << "\n";
+	}
+	return 0;
+}  // } Driver Code Ends
